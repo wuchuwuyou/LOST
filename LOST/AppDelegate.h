a29bd585e94680c63361a513bdcc47a78e9f5e3b
wang teng <wangteng@lenovocloud.com>
2018-03-22 17:43:59 +0800

    init

---
+++ LOST/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOST
+//
+//  Created by Murphy on 22/03/2018.
+//  Copyright © 2018 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LOST/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LOST
+//
+//  Created by Murphy on 22/03/2018.
+//  Copyright © 2018 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
